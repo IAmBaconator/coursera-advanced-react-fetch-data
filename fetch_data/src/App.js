d6ba0ec3f4a37f4aa18f1b1f7359be4492f6fe4e
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2026-02-05 20:27:58 +0000

    Added RandomUser values.

--- fetch_data/src/App.js
+++ fetch_data/src/App.js
@@ -14,11 +14,20 @@ function App() {
   return Object.keys(user).length > 0 ? (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Customer data</h1>
+        <h2>Name: {user.results[0].name.first} {user.results[0].name.last}</h2>
+        <img
+          src={user.results[0].picture.large}
+          title={user.results[0].name.first} {...user.results[0].name.last}
+        />
       </header>
     </div>
   ) : (
-    <h1>Data pending...</h1>
+     <div className="App">
+      <header className="App-header">
+        <h1>Data pending...</h1>
+      </header>
+    </div>
   );
 }
 
